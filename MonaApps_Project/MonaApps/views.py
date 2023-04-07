4727b4a3969cf8e4273631cfec07f426784b89ab
shymiY3 <szymonjanski1@gmail.com>
2023-04-08 00:24:42 +0200

    Urls added

--- MonaApps_Project/MonaApps/views.py
+++ MonaApps_Project/MonaApps/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def MonaApps(request):
     return HttpResponse("Hello world!")
+
+def index(request):
+    return render(request, 'index.html')
+
+def form(request):
+    return render(request, 'Monitoring_form.html')
