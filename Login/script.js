e61b02c769fca8783a63d1dcda952ec1946f8e82
Thang97pl <257698@student.pwr.edu.pl>
2023-04-08 13:16:27 +0200

    added password strength checker for register box. still have problems with the text line being in same line with term and condition check box

--- Login/script.js
+++ Login/script.js
@@ -51,34 +51,92 @@ phoneInput.addEventListener('keydown', function(event) {
 });
 
 // password requierments for register
-const passwordInput = document.getElementById('password-input');
-const passwordError = document.querySelector('.password-error');
-const passwordErrorMessage = document.querySelector('.password-error .error-message');
-const passwordErrorTooltip = document.querySelector('.password-error .tooltip');
-const passwordErrorLength = document.querySelector('.password-error .length');
-const passwordErrorUppercase = document.querySelector('.password-error .uppercase');
-const passwordErrorLowercase = document.querySelector('.password-error .lowercase');
-const passwordErrorNumber = document.querySelector('.password-error .number');
+let input = document.querySelector('#passwordChecker')
+let formGroup = document.querySelector('.form-group')
+let message = document.querySelector('.message')
+let passTypeToggle = document.querySelector('.passTypeToggle')
+let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
 
-passwordInput.addEventListener('input', function() {
-  const password = passwordInput.value;
-  const isValidLength = password.length >= 8;
-  const hasUppercase = /[A-Z]/.test(password);
-  const hasLowercase = /[a-z]/.test(password);
-  const hasNumber = /\d/.test(password);
-
-  if (!isValidLength || !hasUppercase || !hasLowercase || !hasNumber) {
-    passwordError.classList.add('active');
-
-    passwordErrorLength.classList.toggle('done', isValidLength);
-    passwordErrorUppercase.classList.toggle('done', hasUppercase);
-    passwordErrorLowercase.classList.toggle('done', hasLowercase);
-    passwordErrorNumber.classList.toggle('done', hasNumber);
-
-    passwordErrorTooltip.style.display = 'inline';
-  } else {
-    passwordError.classList.remove('active');
-    passwordErrorTooltip.style.display = 'none';
-  }
+document.body.addEventListener('click', function (e) {
+    if (input.contains(e.target)) {
+        formGroup.classList.add('focus')
+    } else {
+        if(input.value == ''){
+            formGroup.classList.remove('focus')
+        }
+    }
 });
 
+let checkPasswordStrength = (password) => {
+    let message = {}
+
+    if(strongPassword.test(password)) {
+        message = {
+            strength : 'strong'
+        }
+    } else if(mediumPassword.test(password)) {
+        message = {
+            strength : 'medium'
+        }
+    } else {
+        message = {
+            strength : 'weak'
+        }
+    }
+    return message
+}
+
+input.addEventListener('keyup', e => {
+    let password = e.target.value
+
+    password != "" ? passTypeToggle.style.display = 'block' : passTypeToggle.style.display = 'none'
+
+    if(password == ''){
+        message.classList.remove('weak')
+        message.classList.remove('medium')
+        message.classList.remove('strong')
+
+        formGroup.classList.remove('weak')
+        formGroup.classList.remove('medium')
+        formGroup.classList.remove('strong')
+
+        message.innerHTML = ''
+    }else{
+        let result = checkPasswordStrength(password)
+
+        if(result.strength == 'weak'){
+            message.classList.remove('medium')
+            message.classList.remove('strong')
+            formGroup.classList.remove('medium')
+            formGroup.classList.remove('strong')
+            message.classList.add('weak')
+            formGroup.classList.add('weak')
+            message.innerHTML = 'Your Password is weak.'
+        }else if(result.strength == 'medium'){
+            formGroup.classList.remove('weak')
+            formGroup.classList.remove('strong')
+            message.classList.remove('weak')
+            message.classList.remove('strong')
+            message.classList.add('medium')
+            formGroup.classList.add('medium')
+            message.innerHTML = 'Your Password is medium.'
+        }else{
+            formGroup.classList.remove('weak')
+            formGroup.classList.remove('medium')
+            message.classList.remove('weak')
+            message.classList.remove('medium')
+            message.classList.add('strong')
+            formGroup.classList.add('strong')
+            message.innerHTML = 'Your Password is Strong.'
+        }
+    }
+
+})
+
+passTypeToggle.addEventListener('click', e => {
+    input.getAttribute('type') == 'password' ? input.setAttribute('type', 'text') : input.setAttribute('type', 'password')
+    input.getAttribute('type') == 'password' ? passTypeToggle.setAttribute('title', 'Show') : passTypeToggle.setAttribute('title', 'Hide')
+    document.querySelector('.passTypeToggle i').classList.toggle('fa-eye')
+    document.querySelector('.passTypeToggle i').classList.toggle('fa-eye-slash')
+})
