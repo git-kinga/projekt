32c596152eba85c3c44e70d7c137d36349fb1b42
shymiY3 <szymonjanski1@gmail.com>
2023-04-09 15:49:48 +0200

    MonitoringForm fixup

--- MonaApps_Project/MonaApps/forms.py
+++ MonaApps_Project/MonaApps/forms.py
@@ -6,18 +6,20 @@ from .models import MonitorRequest
 class MonitorForm(forms.Form):
     websiteURL = forms.CharField(label='Website URL', max_length=200)
 
-    def clean_url(self):
-        url = self.cleaned_data['url']
+    def clean_websiteURL(self):
+        url = self.cleaned_data['websiteURL']
         validate_url = URLValidator()
         try:
             validate_url(url)
+            print("Valid")
         except:
+            print("Invalid")
             raise forms.ValidationError('Invalid URL')
         return url
     
     def save(self, user):
-        url = self.cleaned_data['url']
+        url = self.cleaned_data['websiteURL']
         current_time = timezone.now()
-        url_obj = MonitorRequest(user=user, url=url, date=current_time)
+        url_obj = MonitorRequest(user=user, URL=url, date=current_time)
         url_obj.save()
         return url_obj
--- MonaApps_Project/MonaApps/views.py
+++ MonaApps_Project/MonaApps/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django import forms
 from .forms import MonitorForm
 
 def MonaApps(request):
@@ -9,13 +10,24 @@ def MonaApps(request):
 def index(request):
     return render(request, 'index.html')
 
-@login_required
+#@login_required
 def form(request):
     if request.method == 'POST':
         form = MonitorForm(request.POST)
-        if form.is_valid():
-            form.save(request.user)
+        try:
+            print(form.is_valid())
+            if form.is_valid():
+                print("Inside if")
+                form.save(request.user)
+                return redirect('/')
+        except forms.ValidationError as e:
+            print("erorr views")
+            form.add_error('websiteURL', e.get_messages()[0])
+            return render(request, 'Monitoring_form.html', {'form':form})
+        else:
+            print("else")
             return redirect('/')
+
     else:
         form =MonitorForm()
-    return render(request, 'Monitoring_form.html')
+    return render(request, 'Monitoring_form.html', {'form':form})
