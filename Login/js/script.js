2419bc1d07b04899ecaabd18821a5d38e890c597
Thang97pl <257698@student.pwr.edu.pl>
2023-04-12 21:30:07 +0200

    separted and sorted files

---
+++ Login/js/script.js
@@ -0,0 +1,45 @@
+const logregBox = document.querySelector('.logreg-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const forgotLink = document.querySelector('.forgot-link');
+
+
+// To toggle between login and register
+forgotLink.addEventListener('click', () => {
+    logregBox.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    logregBox.classList.remove('active');
+});
+
+//Janek's code 
+function signIn() {
+  // Get the values of the email and password fields
+  var email = document.getElementById("floatingInput").value;
+  var password = document.getElementById("floatingPassword").value;
+
+  // Create an object with the email and password
+  var credentials = {
+    email: email,
+    password: password
+  };
+
+  // Send a POST request with the credentials in JSON format
+  fetch('/api/signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  })
+  .then(response => {
+    // Handle the response from the server
+    // For example, you can redirect the user to another page
+    window.location.href = '/dashboard';
+  })
+  .catch(error => {
+    // Handle the error
+    console.error('Error:', error);
+  });
+}
