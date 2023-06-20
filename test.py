5c590b94fc33704af07ae5e2b60804f35baf494f
ShymiY3 <116392016+ShymiY3@users.noreply.github.com>
2023-06-20 20:18:23 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://localhost:8000/api/config/user01'
-
-token = 'wGWAGew4DUGaxPQGt5x5bBhfKRjFbsaM'
-
-req = requests.post(url, headers={'Authorization' : token})
-print(req.text)
